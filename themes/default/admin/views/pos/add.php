fcafdada48b0a84db88605b28e5d9ef04221ab54
Mohammad Al-Nur Sarwer Arif <alnurarif@yahoo.com>
2023-10-08 19:07:18 +0600

    it hides the arrow from the number type field

--- themes/default/admin/views/pos/add.php
+++ themes/default/admin/views/pos/add.php
@@ -34,6 +34,17 @@
     <?php }
     ?>
     <style> 
+    /* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
     .swal2-popup{
         border-radius : 0px !important;
     }
